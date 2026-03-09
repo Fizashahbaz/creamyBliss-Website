b6607dc20136359b68938681b07cc11e6f937d32
fiza <sfiza5650@gmail.com>
2026-03-09 22:41:06 +0500

    changes some files

--- app.js
+++ app.js
@@ -24,6 +24,63 @@ btn.addEventListener("click",()=>{
 });
 })
 
+let btn1= document.getElementById("btn1")
+btn1.addEventListener("click",()=>{
+ Swal.fire({
+  title: "your order successfully received😊",
+   text: "Thank you for visiting our website and buying icecream❤😊",
+color:"black",
+    confirmButtonText: "OK",  
+  confirmButtonColor: "#ec4899",
+
+  
+});
+});
+
+
+function orderNow() {
+  Swal.fire({
+    title: 'Place Your Order',
+    html: `
+      <input type="text" id="name" class="swal2-input" placeholder="Your Name ">
+      <input type="email" id="email" class="swal2-input" placeholder="Your Email">
+    `,
+
+      background: 'white',
+      color:"black",
+    confirmButtonText: 'Submit',
+      confirmButtonColor: '#9a1c55',
+    preConfirm: () => {
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+
+      if (!name || !email) {
+        Swal.showValidationMessage('Please enter name and email');
+    
+      }
+
+      return { name: name, email: email };
+    }
+  }).then((result) => {
+    if (result.isConfirmed) {
+  
+            Swal.fire({
+        title: `Thanks ${result.value.name}! 🎉`,
+        html: `<p style="font-size:16px; color:#fff;">Your order has been successfully received. We will contact you soon. 🍦</p>`,
+        background: 'linear-gradient(135deg, #f78ca0, #f9748f)',
+        color: '#fff',
+        icon: 'success',
+        iconColor: '#fff',
+        confirmButtonText: 'OK!',
+        confirmButtonColor: '#9a1c55',
+        customClass:{
+          title:'swal2-title',
+        }
+      });
+    }
+  });
+}
+
 
 
 let togglebtn=document.getElementById("togglebtn")
@@ -43,6 +100,5 @@ togglebtn.addEventListener("click",()=>{
     }
     isDarkMode =!isDarkMode
 })
-
 
 
